e5f32d78e9d4d08f1a052741364f938b1a2573f8
wrossiter <wrossiter@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-04-25 00:26:10 +0000

    FEATURE: added google map selectable field module. Allows a user to input an address or select an address via a google map
    git-svn-id: http://svn.silverstripe.com/open/modules/googlemapselectionfield/trunk@75103 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ code/EditableGoogleMapSelectableField.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Shows a google map on the front end, allowing the user to move
+ * and drag the marker round to select a point then saves this point
+ * in a hidden field for the form submission
+ *
+ */
+
+class EditableGoogleMapSelectableField extends EditableFormField {
+	
+	static $singular_name = 'Google Map';
+	
+	static $plural_name = 'Google Maps';
+	
+	static $api_key = "";
+	
+	public function Icon()  {
+		return 'googlemapselectionfield/images/' . strtolower($this->class) . '.png';	
+	}
+	
+	public function ExtraOptions() {
+		$zoomLevels = array();
+		for($i = 1; $i < 20; $i++) {
+			$message = ($i == 1) ? _t('EditableFormField.LOWEST', 'Lowest') : "";
+			$message = ($i == 19) ? _t('EditableFormField.HIGHEST', 'Highest') : $message;
+			$zoomLevels[$i] = $i . ' - '. $message;
+		}
+		return new FieldSet(
+			new TextField(
+				"Fields[$this->ID][CustomSettings][StartLant]", _t('EditableFormField.STARTLATITUDE', 'Starting Point Latitude'), 
+				$this->getSetting('StartLant')
+			),
+			new TextField(
+				"Fields[$this->ID][CustomSettings][StartLong]", _t('EditableFormField.STARTLONGITUDE', 'Starting Point Longitude'),
+				$this->getSetting('StartLong')
+			),
+			new DropdownField(
+				"Fields[$this->ID][CustomSettings][StartZoom]", _t('EditableFormField.STARTZOOM', 'Starting Zoom Level'),
+				$zoomLevels,
+				$this->getSetting('StartZoom')
+			),
+			new TextField(
+				"Fields[$this->ID][CustomSettings][MapWidth]", _t('EditableFormField.MAPWIDTH', 'Map Width'),
+				$this->getSetting('MapWidth')
+			),
+			new TextField(
+				"Fields[$this->ID][CustomSettings][MapHeight]", _t('EditableFormField.MAPHEIGHT', 'Map Height'),
+				$this->getSetting('MapHeight')				
+			)
+		);
+	}
+	public function getFormField() {
+		$lat = $this->getSetting('StartLant');
+		$long = $this->getSetting('StartLong');
+		$width = ($this->getSetting('MapWidth')) ? $this->getSetting('MapWidth') : 300;
+		$height = ($this->getSetting('MapHeight')) ? $this->getSetting('MapHeight') : 300;
+		Requirements::javascript("http://maps.google.com/maps?file=api&amp;v=2&amp;key=". self::$api_key ."&sensor=true");
+		Requirements::customScript(<<<JS
+			$(document).ready(function() {
+				
+				// default values
+				var map = new GMap2(document.getElementById("map"));
+				var center = new GLatLng($lat, $long);
+				var geocoder = new GClientGeocoder();
+				var marker = new GMarker(center, {draggable: true});
+
+				GEvent.addListener(marker, "dragend", function(overlay, point) {
+					var point = marker.getLatLng();
+					map.setCenter(point);
+					geocoder.getLocations(new GLatLng(point.y, point.x), function(response) {
+						if(response.Status.code == 200) {
+							$("input.googleMapAddressField").val(response.Placemark[0].address);
+							$("input[name=$this->Name]").val(response.Placemark[0].address);
+						}
+					});
+				});
+				
+				map.setCenter(center, 13);
+				map.addOverlay(marker);
+				map.addControl(new GMenuMapTypeControl());
+				map.addControl(new GSmallZoomControl3D());
+				
+				$("input.googleMapAddressField").focus(function() {
+					$(this).val("");
+				});
+				$("input.googleMapAddressSubmit").click(function() {
+					var address = $("input.googleMapAddressField").val();
+				 	geocoder.getLatLng(
+				 		address,
+				 		function(point) {
+				 			if (!point) {
+				 				alert(address + " not found");
+				 			} else {
+				 				map.setCenter(point,16);
+				 				marker.setPoint(point);
+				 			}
+				 		}
+					);
+					return false;
+				});
+			});
+JS
+);
+		return new GoogleMapSelectableField($this->Name, $this->Title);
+	}
+}
+?>
---
+++ code/GoogleMapSelectableField.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * A form field which outputs a google map, input field for an address which moves
+ * the map.
+ *
+ * Currently saves the address of a given point. 
+ *
+ * @todo Save the Long, Lat fields as well
+ * @package googlemapselectionfield
+ */
+
+class GoogleMapSelectableField extends FormField {
+	
+	protected $width = "300px";
+	
+	protected $height = "300px";
+	
+	/**
+	 * Set the width of this field
+	 *
+	 * @param String Width
+	 */
+	public function setWidth($width) {
+		$this->width = $width;
+	}
+	
+	/**
+	 * Set the height of this field
+	 *
+	 * @param String 
+	 */
+	public function setHeight($height) {
+		$this->height = $height;
+	}
+	
+	function Field() {
+		return "
+			<div class=\"field text\">
+			<label class=\"left\">$this->Title</label>
+			<input type=\"text\" id=\"{$this->id()}\" name=\"{$this->name()}\" value=\"". _t('EditableFormField.ENTERADDRESS', 'Enter Address') ."\" class=\"text googleMapAddressField\"/>
+			<input type=\"submit\" value=\"". _t('EditableFormField.GO', 'Go') ."\" class=\"submit googleMapAddressSubmit\" />
+			<div id=\"map\" style=\"width: $this->width; height: $this->height;\"></div>";
+	}
+}
+?>
