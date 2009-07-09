ebccd3286b0e4982c0f14a134d253be94f5906f7
wrossiter <wrossiter@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-07-09 20:52:01 +0000

    MINOR: updated documentation
    git-svn-id: http://svn.silverstripe.com/open/modules/googlemapselectionfield/trunk@81491 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/GoogleMapSelectionField.js
+++ javascript/GoogleMapSelectionField.js
@@ -1,45 +1,47 @@
-$(document).ready(function() {
+(function($) {
+	$(document).ready(function() {
 	
-	// default values
-	var map = new GMap2(document.getElementById("map_$Name"));
-	var center = new GLatLng($DefaultLat, $DefaultLon);
-	var geocoder = new GClientGeocoder();
-	var marker = new GMarker(center, {draggable: true});
+		// default values
+		var map = new GMap2(document.getElementById("map_$Name"));
+		var center = new GLatLng($DefaultLat, $DefaultLon);
+		var geocoder = new GClientGeocoder();
+		var marker = new GMarker(center, {draggable: true});
 
-	map.setCenter(center, $Zoom);
-	map.addOverlay(marker);
-	map.addControl(new GMenuMapTypeControl());
-	map.addControl(new GSmallZoomControl3D());
+		map.setCenter(center, $Zoom);
+		map.addOverlay(marker);
+		map.addControl(new GMenuMapTypeControl());
+		map.addControl(new GSmallZoomControl3D());
 
-	GEvent.addListener(marker, "dragend", function(overlay, point) {
-		var point = marker.getLatLng();
-		map.setCenter(point);
-		geocoder.getLocations(new GLatLng(point.y, point.x), function(response) {
-			if(response.Status.code == 200) {
-				$("input[name=$Name]").val(response.Placemark[0].address);
-				$("input[name=$Name_MapURL]").val("http://maps.google.com/?ll=" + point.toUrlValue() +"&q="+ point.toUrlValue() +"&z="+ map.getZoom());
+		GEvent.addListener(marker, "dragend", function(overlay, point) {
+			var point = marker.getLatLng();
+			map.setCenter(point);
+			geocoder.getLocations(new GLatLng(point.y, point.x), function(response) {
+				if(response.Status.code == 200) {
+					$("input[name=$Name]").val(response.Placemark[0].address);
+					$("input[name=$Name_MapURL]").val("http://maps.google.com/?ll=" + point.toUrlValue() +"&q="+ point.toUrlValue() +"&z="+ map.getZoom());
+				}
+			});
+		});
+
+		$("input[name=$Name]").focus(function() {
+			if($(this).val() == $(this).attr("value")) {
+				$(this).val("");	
 			}
 		});
+		$("input.googleMapAddressSubmit").click(function() {
+			var address = $("input.googleMapAddressField").val();
+		 	geocoder.getLatLng(
+		 		address,
+		 		function(point) {
+		 			if (!point) {
+		 				alert(address + " not found");
+		 			} else {
+		 				map.setCenter(point,16);
+		 				marker.setPoint(point);
+		 			}
+		 		}
+			);
+			return false;
+		});
 	});
-
-	$("input[name=$Name]").focus(function() {
-		if($(this).val() == $(this).attr("value")) {
-			$(this).val("");	
-		}
-	});
-	$("input.googleMapAddressSubmit").click(function() {
-		var address = $("input.googleMapAddressField").val();
-	 	geocoder.getLatLng(
-	 		address,
-	 		function(point) {
-	 			if (!point) {
-	 				alert(address + " not found");
-	 			} else {
-	 				map.setCenter(point,16);
-	 				marker.setPoint(point);
-	 			}
-	 		}
-		);
-		return false;
-	});
-});
+})(jQuery);
